330d336b6f46f248835bb4c86484429740b33c01
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-30 11:06:14 +0700

    Class Basic And Instance

---
+++ src/main/kotlin/com/ahmd/Person.kt
@@ -0,0 +1,7 @@
+package com.ahmd
+
+class Person (val firstName: String, val lastName: String, val age: Int) {
+    init {
+        println("Hallo nama saya $firstName $lastName dan saya berumur $age tahun.")
+    }
+}
---
+++ src/main/kotlin/instances.kt
@@ -0,0 +1,5 @@
+import com.ahmd.Person
+
+fun main() {
+    val person1 = Person("Ahmad","Shaleh", 18)
+}
